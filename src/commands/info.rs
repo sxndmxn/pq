8c5c24e35f59cbd89831b06428535259ae19b933
SANDMAC <sandmac@SANDMACs-Mac-mini.local>
2026-05-27 05:22:04 -0700

    refactor: separate structured info output

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -14,13 +14,13 @@ pub fn run(args: InfoArgs) -> Result<()> {
     let dataset = Dataset::from_inputs(inputs)?;
     let output_format: output::OutputFormat = output.into();
     let infos = api::info(&dataset)?;
-    if output_format.is_table() {
+    if let Some(structured_output) = output_format.structured() {
+        output::write_file_infos(structured_output, quiet, &infos)
+    } else {
         for info in &infos {
             commands::print_source_header(&dataset, info.path(), quiet);
-            output::write_file_infos(output_format, quiet, std::slice::from_ref(info))?;
+            output::write_file_info(quiet, info)?;
         }
         Ok(())
-    } else {
-        output::write_file_infos(output_format, quiet, &infos)
     }
 }
